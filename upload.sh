478660e1f35946ca7e851cb3ab51d6ef3295436a
MegaThorx <megathorx@gmail.com>
2019-04-24 14:37:01 +0200

    Updated CI

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 cd build
+ls -l
 export UPLOAD_STR=$(curl --request POST --header "PRIVATE-TOKEN: $API_TOKEN" --form "file=@at.megathorx.wsc-api.tar" https://git.heisi.at/api/v4/projects/$CI_PROJECT_ID/uploads)
 export UPLOAD_FILE=$(echo $UPLOAD_STR | grep -oP '"markdown":"([^"]{1,})"' | sed 's/"markdown":"//g' | sed 's/"//g')
 export UPLOAD_DATA="description=$UPLOAD_FILE"
