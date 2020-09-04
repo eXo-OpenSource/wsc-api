c9d8e17de963198618992980d334cc4f9dc07df1
MegaThorx <git@merx.dev>
2020-09-04 16:41:12 +0200

    Fix CI

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
 cd build
 ls -l
-export UPLOAD_STR=$(curl --request POST --header "PRIVATE-TOKEN: $API_TOKEN" --form "file=@at.megathorx.wsc_api.tar" https://git.heisi.at/api/v4/projects/$CI_PROJECT_ID/uploads)
+export UPLOAD_STR=$(curl --request POST --header "PRIVATE-TOKEN: $API_TOKEN" --form "file=@at.megathorx.wsc_api.tar" https://git.exo.cool/api/v4/projects/$CI_PROJECT_ID/uploads)
 export UPLOAD_FILE=$(echo $UPLOAD_STR | grep -oE '"markdown":"([^"]{1,})"' | sed 's/"markdown":"//g' | sed 's/"//g')
 export UPLOAD_DATA="description=$UPLOAD_FILE"
 echo $UPLOAD_STR
 echo $UPLOAD_FILE
 echo $UPLOAD_DATA
-echo $(curl --request POST --header "PRIVATE-TOKEN: $API_TOKEN" -d $UPLOAD_DATA https://git.heisi.at/api/v4/projects/$CI_PROJECT_ID/repository/tags/$CI_COMMIT_REF_NAME/release)
+echo $(curl --request POST --header "PRIVATE-TOKEN: $API_TOKEN" -d $UPLOAD_DATA https://git.exo.cool/api/v4/projects/$CI_PROJECT_ID/repository/tags/$CI_COMMIT_REF_NAME/release)
